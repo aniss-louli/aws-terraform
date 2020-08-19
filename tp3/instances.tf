6a732c208d445d73c828f88dcee30a2426cd75cd
aniss-louli <57408333+aniss-louli@users.noreply.github.com>
2020-08-20 01:14:30 +0200

    Update instances.tf

--- tp3/instances.tf
+++ tp3/instances.tf
@@ -8,8 +8,8 @@ resource "aws_key_pair" "tfkeypair10" {
 }
 
 # Adapter le nom à l'usage
-resource "aws_security_group" "reseau_interne" {
-   name = "reseau_interne"
+resource "aws_security_group" "reseau_interne_W" {
+   name = "reseau_interne_W"
    vpc_id = aws_vpc.reseau_interne.id
    # en entrée
    # autorise communication avec MariaDB
@@ -19,32 +19,54 @@ resource "aws_security_group" "reseau_interne" {
      protocol  = "-1" # -1 = pour tout les protocoles
      cidr_blocks = [ "192.168.77.20/32" ]
    }
-   # autorise communication avec Wordpress
-   ingress {
-     from_port = "0"
-     to_port   = "0"
-     protocol  = "-1"
-     cidr_blocks = [ "192.168.77.10/32" ]
-   }
-   # autorise http de partout
+    # autorise http de partout
    ingress {
      from_port = "80"
      to_port   = "80"
      protocol  = "tcp"
      cidr_blocks = [ "0.0.0.0/0" ]
    }
+  # autorise ssh de partout
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_security_group" "reseau_interne_M" {
+   name = "reseau_interne_M"
+   vpc_id = aws_vpc.reseau_interne.id
+   # en entrée
+   # autorise communication avec Wordpress
+   ingress {
+     from_port = "3306"
+     to_port   = "3306"
+     protocol  = "tcp"
+     cidr_blocks = [ "192.168.77.10/32" ]
+   }
+   # autorise ssh de partout
+   ingress {
+     from_port   = "22"
+     to_port     = "22"
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+}
+
+   
 
 resource "aws_instance" "Wordpress" {
    # Ubuntu 18.04 fournie par AWS
    ami = "ami-0bcc094591f354be2"
    instance_type = "t2.micro"
    key_name = "tfkeypair10"
-   vpc_security_group_ids = [ aws_security_group.reseau_interne.id ]
+   vpc_security_group_ids = [ aws_security_group.reseau_interne_W.id ]
    subnet_id = aws_subnet.subnet_example.id
    private_ip = "192.168.77.10"
    associate_public_ip_address = "true"
-   user_data = file("instance_init1.sh")
+   user_data = file("./instance_init1.sh")
    tags = {
      Name = "Wordpress"
    }
@@ -55,11 +77,11 @@ resource "aws_instance" "MariaDB" {
    ami = "ami-0bcc094591f354be2"
    instance_type = "t2.micro"
    key_name = "tfkeypair10"
-   vpc_security_group_ids = [ aws_security_group.reseau_interne.id ]
+   vpc_security_group_ids = [ aws_security_group.reseau_interne_M.id ]
    subnet_id = aws_subnet.subnet_example.id
    private_ip = "192.168.77.20"
    associate_public_ip_address = "true"
-   user_data = file("instance_init2.sh")
+   user_data = file("./instance_init2.sh")
    tags = {
      Name = "MariaDB"
    }
