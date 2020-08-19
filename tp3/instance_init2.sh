2ec8182ba1b3979f813ad392493f1f5ae5aa5d82
aniss-louli <57408333+aniss-louli@users.noreply.github.com>
2020-08-19 23:38:33 +0200

    Update instance_init2.sh

--- tp3/instance_init2.sh
+++ tp3/instance_init2.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-apt -y update && apt -y upgrade
+sudo apt -y update && apt -y upgrade
 sudo apt install mariadb-server -y
+sudo mysql -e "CREATE DATABASE wordpress CHARACTER SET UTF8 COLLATE UTF8_BIN"
+sudo mysql -e "CREATE USER 'ubuntu'@'%' IDENTIFIED BY 'wordpress'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO ubuntu@192.168.77.10 IDENTIFIED by 'wordpress'"
+sudo mysql -e "FLUSH PRIVILEGES"
+sudo sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mysql/my.cnf
+sudo systemctl restart mariadb.service                                     
