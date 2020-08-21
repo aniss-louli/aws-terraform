45bc7fbbec96bff2ec112f584341a7f4d4727f63
aniss-louli <57408333+aniss-louli@users.noreply.github.com>
2020-08-21 15:10:34 +0200

    Update init_windows.sh

--- tp6/init_windows.sh
+++ tp6/init_windows.sh
@@ -1,4 +1,14 @@
+#ajout d'un admin
 net user aniss /add /y
-net user aniss M0tdeP@$$e
+net user aniss Password123
 net localgroup administrators aniss /add
 echo ${base64encode(file("./test.txt"))} > tmp2.b64 && certutil -decode tmp2.b64 C:/test.txt
+#configurer le Bureau à distance
+remoteusercredentials = Get-Credential
+ConvertTo-SecureString -String "Password123" -AsPlainText -Force | ConvertFrom-SecureString | Set-Content "password.txt"
+$servicename = "cloudservice"
+$username = "RemoteDesktopUser"
+$securepassword = Get-Content -Path "password.txt" | ConvertTo-SecureString
+$expiry = $(Get-Date).AddDays(1)
+$credential = New-Object System.Management.Automation.PSCredential $username,$securepassword
+Set-AzureServiceRemoteDesktopExtension -ServiceName $servicename -Credential $credential -Expiration $expiry
